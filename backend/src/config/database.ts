4f5c102489ee798e4d828f57accf115cad2c95c8
unknown <guilleromanm@gmail.com>
2025-04-02 16:52:27 +0200

    Connecting the dots for the new backend container setup. BE working

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,19 +1,36 @@
 // config/database.ts
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+import { URL } from 'url'; // Import the URL module
 
 dotenv.config();
 
-const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  user: process.env.DB_USER || 'user',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'todo_db',
-  max: parseInt(process.env.DB_POOL_MAX || '10', 10),
-  idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT || '30000', 10),
-  connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT || '2000', 10)
-};
+let dbConfig = {};
+// Try reading .env, else set default values.
+if (process.env.DATABASE_URL) {
+    const dbUrl = new URL(process.env.DATABASE_URL);
+    dbConfig = {
+        host: dbUrl.hostname,
+        port: dbUrl.port ? parseInt(dbUrl.port, 10) : 5432,
+        user: dbUrl.username,
+        password: dbUrl.password,
+        database: dbUrl.pathname.substring(1),
+        max: parseInt(process.env.DB_POOL_MAX || '10', 10),
+        idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT || '30000', 10),
+        connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT || '2000', 10)
+    };
+} else {
+    dbConfig = {
+        host: process.env.DB_HOST || 'postgres-db', // Use postgres-db as default
+        port: parseInt(process.env.DB_PORT || '5432', 10),
+        user: process.env.DB_USER || 'user',
+        password: process.env.DB_PASSWORD || 'password',
+        database: process.env.DB_NAME || 'todo_db',
+        max: parseInt(process.env.DB_POOL_MAX || '10', 10),
+        idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT || '30000', 10),
+        connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT || '2000', 10)
+    };
+}
 
 // Create and export the pool instance
 const pool = new Pool(dbConfig);
